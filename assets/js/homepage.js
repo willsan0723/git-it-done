04149d3212eaab06e4ba9ff55974936dd8189af4
willsan0723 <william.santee@gmail.com>
2020-09-01 18:43:56 -0600

    verifying homepage.js was updated correctly

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -29,7 +29,11 @@ var getUserRepos = function (user) {
             });
         } else {
             alert("Error: " + response.statusText);
-        };
+        }
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect to GitHub");
     });
     };
 
